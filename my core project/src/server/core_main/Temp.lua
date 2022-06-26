cfd17cfcbcfd9bda3b67016123861ca10c9676ae
morganic <108112378+morginc-1@users.noreply.github.com>
2022-06-25 21:08:16 -0700

    wow I'm dumb
    fixed the file name not being correctly spelled.

---
+++ my core project/src/server/core_main/Temp.lua
@@ -0,0 +1,35 @@
+Parent.numbervalue.power =1 == 100
+Parent.numbervalue.power =2 == 150
+Parent.numbervalue.power =3 ==200
+Parent.numbervalue.cooling =1 ==-101
+Parent.numbervalue.cooling =2 ==-151
+Parent.numbervalue.cooling =3 ==-201
+if script.Parent.numbervalue.power == 1 then
+	numbervalue.Heat = value.power1 + numbervalue.Heat
+
+end
+
+if script.Parent.numbervalue.power == 2 then
+	numbervalue.Heat = value.power == 2 + numbervalue.Heat
+
+end
+
+if script.Parent.numbervalue.power == 3 then
+	numbervalue.Heat = value.power == 3 + numbervalue.Heat
+
+end
+
+if script.Parent.numbervalue.cooling == 1 then
+	numbervalue.Heat = value.cooling == 1 + numbervalue.Heat
+
+end
+
+if script.Parent.numbervalue.cooling == 2 then
+	numbervalue.Heat = value.cooling == 2 + numbervalue.Heat
+
+end
+
+if script.Parent.numbervalue.cooling == 3 then
+	numbervalue.Heat = value.cooling == 3 + numbervalue.Heat
+
+end
---
+++ my core project/src/server/core_main/cooling.lua
@@ -0,0 +1,1 @@
+numbervalue.Instance.new(classname:cooling"numbervalue")
